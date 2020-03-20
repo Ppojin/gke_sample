7651f42ea0539fb856654e1d170b2f2bbdfef437
Ppojin <25076967+Ppojin@users.noreply.github.com>
2020-03-20 13:27:33 +0900

    [mod] runsh

--- polls-app-gke/run.sh
+++ polls-app-gke/run.sh
@@ -3,7 +3,7 @@
 echo "\n========== Creating the secrets ==========\n"
 kubectl create secret generic mysql-root-pass --from-literal=password=R00t
 kubectl create secret generic mysql-user-pass --from-literal=username=callicoder --from-literal=password=c@ll1c0d3r
-kubectl create secret generic mysql-db-url --from-literal=database=polls --from-literal=url='jdbc:mysql://polling-app-mysql:3306/polls?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false'
+kubectl create secret generic mysql-db-url --from-literal=database=polls --from-literal=url='jdbc:mysql://polling-app-mysql:3306/polls?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false&autoReconnection=true'
 
 echo "\n========== Deploying MySQL ==========\n"
 kubectl apply -f deployments/mysql-deployment.yaml
@@ -14,5 +14,5 @@ kubectl apply -f deployments/polling-app-server.yaml
 echo "\n========== Deploying the React app on Kubernetes ==========\n"
 kubectl apply -f deployments/polling-app-client.yaml
 
-echo "\n========== ingress ==========\n"
-kubectl apply -f deployments/ingress.yaml
+# echo "\n========== ingress ==========\n"
+# kubectl apply -f deployments/ingress.yaml
